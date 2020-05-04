55e43d7ce7520e0c15d0feec7afca509cf22fe45
Guilherme Marcello <58813280+guilherme-marcello@users.noreply.github.com>
2020-05-04 09:41:52 +0100

    Update float.go

--- geral/2.DataTypes/numericos/float.go
+++ geral/2.DataTypes/numericos/float.go
@@ -1,14 +1,16 @@
 package main
 import "fmt"
-// float64 é designado double em C/C++/Java
-// float32 normalmente é designado apenas por float
+// float64 é designado por double em C/C++/Java
+// float32 normalmente é designado apenas por float ou single
 // Em Go, exite float32 e float64.
 
-// float32 -> ocupa 32 bits de memória
-// float64 -> ocupa 64 bits de memória
+// float32 -> ocupa 32 bits de memória; é mais rápido
+// float64 -> ocupa 64 bits de memória; é mais preciso
 
 func main(){
 	var valor_flutuante float64 = 1.2
 	nome := "Exemplo de Float:"
 	fmt.Println(nome,valor_flutuante)
 }
+
+// Ler: https://en.wikipedia.org/wiki/IEEE_754#Basic_and_interchange_formats
